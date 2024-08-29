6c835b20139ae202b7700f010dfa3adb45aba081
Elyas Tigre <elyastigre@gmail.com>
2024-08-29 11:27:23 +0200

    Notes

---
+++ Runtime/Helpers/Note.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    /**
+     * Meant to add a notes field to inspector
+     */
+    public class Note : MonoBehaviour
+    {
+        [TextArea(3, 20)] public string note;
+    }
+}

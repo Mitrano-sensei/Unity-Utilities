b3913bda715f533b10632890f215118b6b14277d
Mitrano <elyastigre@gmail.com>
2026-03-22 15:19:48 +0100

    TransformHelper

---
+++ Runtime/Helpers/TransformHelpers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public static class TransformHelpers
+    {
+        public static void DestroyChildren(this Transform t)
+        {
+            foreach (Transform child in t)
+            {
+                Object.DestroyImmediate(child.gameObject);
+            }
+        }
+    }
+}
